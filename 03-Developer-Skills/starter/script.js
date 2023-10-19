e448efb136a8792a203a8835c2a13e9c7df93c44
Tom-Apted <tapted0@gmail.com>
2023-10-20 00:18:36 +0100

    finnished task

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,6 +1,6 @@
 // Remember, we're gonna use strict mode in all scripts now!
 "use strict";
-
+/*
 const temperatures = [3, -2, -6, -1, "error", 9, 13, 17, 15];
 const beepboop = [14, 9, 5];
 
@@ -19,3 +19,27 @@ const calcTempAmplitude = function (temps1, temps2) {
 
 const amp = calcTempAmplitude(temperatures, beepboop);
 console.log(amp);
+
+const measureKelvin = function () {
+  const measurement = {
+    type: "temp",
+    unit: "celsius",
+    value: Number(prompt("Degrees celsius")),
+  };
+
+  const kelvin = measurement.value + 273;
+  return kelvin;
+};
+
+console.log(measureKelvin());
+*/
+
+const printForecast = function (arr) {
+  let test = "...";
+  for (let i = 0; i < arr.length; i++) {
+    test = test + `${arr[i]} in ${i + 1} days ... `;
+  }
+  return test;
+};
+
+console.log(printForecast([12, 5, -5, 0, 4]));
