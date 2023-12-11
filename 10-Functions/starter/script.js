1f6e2338992a4ea7550501d8cfdaa700a0dcbc0e
Tom-Apted <tapted0@gmail.com>
2023-12-11 23:21:05 +0000

    made start to section 10

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,1 +1,80 @@
 'use strict';
+/*
+const bookings = [];
+
+const createBooking = function (
+  flightNum,
+  numPassengers = 1,
+  price = 199 * numPassengers
+) {
+  // numPassengers = numPassengers || 1;
+  // price = price || 199;
+
+  const booking = {
+    flightNum,
+    numPassengers,
+    price,
+  };
+  console.log(booking);
+  bookings.push(booking);
+};
+
+createBooking('LH123');
+createBooking('LH123', 2, 800);
+createBooking('LH123', 2);
+createBooking('LH123', 5);
+createBooking('LH123', undefined, 1000);
+
+const flight = 'LH234';
+const jonas = {
+  name: 'jonas shmit',
+  passport: 122346890,
+};
+const checkIn = function (flightNum, passenger) {
+  flightNum = 'lh999';
+  passenger.name = 'mr. ' + passenger.name;
+
+  if (passenger.passport === 122346890) {
+    alert('checked In');
+  } else {
+    alert('wrong passport!');
+  }
+};
+
+checkIn(flight, jonas);
+console.log(flight);
+console.log(jonas);
+
+const newPassport = function (person) {
+  person.passport = Math.trunc(Math.random() * 100000000);
+};
+newPassport(jonas);
+checkIn(flight, jonas);
+*/
+
+const oneWord = function (str) {
+  return str.replace(/ /g, '').toLowerCase();
+};
+
+const upperFirstWord = function (str) {
+  const [first, ...others] = str.split(' ');
+  return [first.toUpperCase(), ...others].join(' ');
+};
+
+const transformer = function (str, fn) {
+  console.log(`original string: ${str}`);
+  console.log(`Transformed string: ${fn(str)}`);
+
+  console.log(`Transformed by: ${fn.name}`);
+};
+
+transformer('JavaScript it the best', upperFirstWord);
+transformer('JavaScript it the best', oneWord);
+
+const high5 = function () {
+  console.log('hi');
+};
+
+document.body.addEventListener('click', high5);
+
+['jonas', 'martha', 'adam'].forEach(high5);
